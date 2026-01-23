b55e0987c491537480d76759cadcfa44ac25ec11
Ula <Ula@DESKTOP-VD5E81I>
2026-01-23 18:53:24 +0100

    Dodaj pliki projektów.

---
+++ gra_rpg/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace gra_rpg
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ gra_rpg/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace gra_rpg;  
+
+public partial class MainPage : ContentPage
+{
+    bool ishovered = false;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void OnPointerEntered(object sender, EventArgs e)
+    {
+        if (ishovered) return;
+        ishovered = true;
+
+        await newGameButton.ScaleToAsync(1.08, 120, Easing.CubicOut);
+    }
+
+    private async void OnPointerExited(object sender, EventArgs e)
+    {
+        ishovered = false;
+
+        await newGameButton.ScaleToAsync(1.0, 120, Easing.CubicIn);
+    }
+
+    private void OnNewGameClicked(object sender, EventArgs e)
+    {
+        // start
+       
+    }
+}
