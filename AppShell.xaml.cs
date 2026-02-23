e879a1b12e6a28df2751235aeeabdd7411a61680
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2026-02-23 08:28:18 +0100

    Add files via upload

---
+++ AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace gra_rpg;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+
+        // Rejestracja trasy, dzięki czemu Shell.Current.GoToAsync(nameof(Page1)) będzie działać
+        Routing.RegisterRoute(nameof(Page1), typeof(Page1));
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace gra_rpg
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
