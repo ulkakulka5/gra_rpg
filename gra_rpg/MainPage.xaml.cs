a45f9e10887b61c9c5e72568d1f93f5c45e7fb9e
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2026-03-08 12:57:19 +0100

    Implement background music and click sound effects
    Added music playback functionality to MainPage.

--- gra_rpg/MainPage.xaml.cs
+++ gra_rpg/MainPage.xaml.cs
@@ -1,16 +1,39 @@
-﻿namespace gra_rpg;  
+namespace gra_rpg;
 
+using Plugin.Maui.Audio;// POBRAC!
 public partial class MainPage : ContentPage
 {
     bool ishovered = false;
-
+    IAudioPlayer? player;
     public MainPage()
     {
         InitializeComponent();
+        PlayMusic();
+    }
+
+
+    async void PlayMusic()
+    {
+        var audioManager = AudioManager.Current;
+
+        var stream = await FileSystem.OpenAppPackageFileAsync("music.mp3");
+
+        player = audioManager.CreatePlayer(stream);
+
+        player.Loop = true;   
+        player.Play();
     }
 
     private async void OnPointerEntered(object sender, EventArgs e)
     {
+        var audioManager = AudioManager.Current;
+
+        var stream = await FileSystem.OpenAppPackageFileAsync("click.mp3");
+
+        player = audioManager.CreatePlayer(stream);
+
+        player.Loop = false;
+        player.Play();
         if (ishovered) return;
         ishovered = true;
 
@@ -27,6 +50,7 @@ public partial class MainPage : ContentPage
     async void OnNewGameClicked(object sender, EventArgs e)
     {
         // start
+        
         await Shell.Current.GoToAsync(nameof(Page1));
 
     }
