dd38922d120b9305e5ad94f40261c66c0a8ec3ce
ulkakulka5 <134429167+ulkakulka5@users.noreply.github.com>
2026-03-20 17:19:52 +0100

    Register routes for Page3 and Page4

--- gra_rpg/AppShell.xaml.cs
+++ gra_rpg/AppShell.xaml.cs
@@ -7,5 +7,7 @@ public partial class AppShell : Shell
         InitializeComponent();
         Routing.RegisterRoute(nameof(Page1), typeof(Page1));
         Routing.RegisterRoute(nameof(Page2), typeof(Page2));
+         Routing.RegisterRoute(nameof(Page3), typeof(Page3));
+ Routing.RegisterRoute(nameof(Page4), typeof(Page4));
     }
 }
